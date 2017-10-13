297e7065099f78cd7a8c178df306f359cda6957b
Paragon <ngoc.k.pham@stud.h-da.de>
2017-10-13 18:05:06 +0200

    implemented "fehlende Programmteile" :smirk:

--- app/src/main/java/h_da/fbi/mikha/myfirstquiz/MainQuizActivity.java
+++ app/src/main/java/h_da/fbi/mikha/myfirstquiz/MainQuizActivity.java
@@ -1,20 +1,23 @@
 package h_da.fbi.mikha.myfirstquiz;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import hochschuledarmstadt.quizapp.QuizActivity;
 
-public class MainActivity extends QuizActivity {
+public class MainQuizActivity extends QuizActivity {
 
     /*
      * TODO: Hier weitere Variablen für die UI Komponenten anlegen
@@ -29,8 +32,10 @@ public class MainActivity extends QuizActivity {
         setContentView(R.layout.activity_main_quiz);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        // TODO: Hier die erzeugten Views über "findViewById(...)" referenzieren
-        // und gegebenenfalls Listener setzen
+
+
+
+
         textView = (TextView) findViewById(R.id.Frage);
         button = (Button) findViewById(R.id.submitButton);
         radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
@@ -38,66 +43,76 @@ public class MainActivity extends QuizActivity {
             @Override
             public void onCheckedChanged(RadioGroup group, int checkedId) {
                 getQuiz().setCheckedRadioButtonId(checkedId);
+
             }
         });
+
+
 
         button.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 getQuiz().submitAnswer();
             }
         });
+
+
     }
 
     /*
-	 * TODO: Methode definert eine mögliche Antwort an den Radio Button
+	 * Methode definert eine mögliche Antwort an den Radio Button
 	 * an Position index
 	 */
     @Override
     public void renderPossibleAnswer(int index, String possibleAnswerText) {
 
+        RadioButton selectedButton = (RadioButton) radioGroup.getChildAt(index);
+        selectedButton.setText(possibleAnswerText);
     }
 
     /*
-	 * TODO: Methode löscht den gewählten Radio Button
+	 * Methode löscht den gewählten Radio Button
 	 */
     @Override
     public void clearCheckedRadioButton() {
-
+        radioGroup.clearCheck();
     }
 
     /*
-	 * TODO: Methode definiert die aktulle Frage in der TextView
+	 * Methode definiert die aktulle Frage in der TextView
 	 */
     @Override
     public void renderQuestion(String questionText) {
+        textView.setText(questionText);
 
     }
 
     /*
-	 * TODO: Methode definiert einen Untertitel, der den Fortschritt
+	 *  Methode definiert einen Untertitel, der den Fortschritt
 	 * des Spiels angibt.
 	 */
     @Override
     public void renderActionBarSubtitle(String subtitleText) {
 
+        setTitle(subtitleText);
     }
 
 
     /*
-	 * TODO: Methode aktiviert den Button zur Übermittlung von Antworten
+	 *  Methode aktiviert den Button zur Übermittlung von Antworten
 	 */
     @Override
     public void setAnswerButtonEnabled() {
-
+        button.setEnabled(true);
     }
 
     /*
-	 * TODO: Methode deaktiviert den Button zur Übermittlung von Antworten
+	 *  Methode deaktiviert den Button zur Übermittlung von Antworten
 	 */
     @Override
     public void setAnswerButtonDisabled() {
-
+        button.setEnabled(false);
     }
 
 
@@ -107,6 +122,7 @@ public class MainActivity extends QuizActivity {
 	 */
     @Override
     public void onQuizEnd(final int correctAnswers, final int wrongAnswers) {
+
 
     }
 
