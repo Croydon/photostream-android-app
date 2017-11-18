acfbf49bf6f7c3765a0dc08a0b10efae3ab38b65
Croydon <cr0ydon@outlook.com>
2017-11-18 22:02:48 +0100

    Add Upload Photo Activity

---
+++ app/src/main/java/h_da/fbi/khami/photostream/UploadPhoto.java
@@ -0,0 +1,13 @@
+package h_da.fbi.khami.photostream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UploadPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+    }
+}
