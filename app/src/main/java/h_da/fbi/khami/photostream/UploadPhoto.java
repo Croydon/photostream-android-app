5a0629088d06eaaf0927425d76cfe75f928999a7
Croydon <cr0ydon@outlook.com>
2017-11-18 22:32:18 +0100

    PhotoUpload: Connect Activity with Menu

--- app/src/main/java/h_da/fbi/khami/photostream/UploadPhoto.java
+++ app/src/main/java/h_da/fbi/khami/photostream/UploadPhoto.java
@@ -2,6 +2,9 @@ package h_da.fbi.khami.photostream;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class UploadPhoto extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class UploadPhoto extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upload_photo);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.upload_photo_item:
+                // FIXME: Upload photo
+
+                return true;
+        }
+
     }
 }
