c603bb172ecbc8fa8a353ec6d70cd712d13f9d60
Croydon <cr0ydon@outlook.com>
2017-11-20 03:45:14 +0100

    Allow taking new photo and receiving photo from sharing actions

--- app/src/main/java/h_da/fbi/khami/photostream/UploadPhoto.java
+++ app/src/main/java/h_da/fbi/khami/photostream/UploadPhoto.java
@@ -1,13 +1,27 @@
 package h_da.fbi.khami.photostream;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class UploadPhoto extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView selectPhotoImageView;
+    Bitmap selectedPhoto;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +29,43 @@ public class UploadPhoto extends AppCompatActivity {
         setContentView(R.layout.activity_upload_photo);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        selectPhotoImageView = (ImageView) findViewById(R.id.select_photo_imageView);
+
+        selectPhotoImageView.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
+
+
+        // Get intent, action and MIME type
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (type.startsWith("image/")) {
+                handleSingleImageIntent(intent, action, type);
+
+            }
+        }
     }
 
+    public void handleSingleImageIntent(Intent intent, String action, String type)
+    {
+        Uri imageUri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        if (imageUri != null) {
+            try {
+                Bitmap getPhotoFromURI = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                setPhoto(getPhotoFromURI); // Handle single image being sent
+            }
+            catch(Exception e)
+            {
+                // FIXME: Give user feedback that the picture could not be loaded
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -33,13 +82,29 @@ public class UploadPhoto extends AppCompatActivity {
             case android.R.id.home:
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
-
-            case R.id.upload_photo_item:
-                // FIXME: Upload photo
-
-                return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            setPhoto((Bitmap) extras.get("data"));
+        }
+    }
+
+    public void setPhoto(Bitmap newPhoto)
+    {
+        selectedPhoto = newPhoto;
+        selectPhotoImageView.setImageBitmap(selectedPhoto);
     }
 }
