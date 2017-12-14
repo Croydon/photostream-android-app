dc6be59f841943a5729a7379b121e04ed2251268
Paragon <ngoc.k.pham@stud.h-da.de>
2017-12-15 00:30:43 +0100

    Layout for adding comment basic

---
+++ app/src/main/java/h_da/fbi/khami/photostream/AddCommentActivity.java
@@ -0,0 +1,14 @@
+package h_da.fbi.khami.photostream;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddCommentActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_comment);
+    }
+}
