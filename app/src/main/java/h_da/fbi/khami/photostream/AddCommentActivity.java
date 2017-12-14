7e4c53965238cbf713c1c43a490cb34e6e867356
Paragon <ngoc.k.pham@stud.h-da.de>
2017-12-15 00:59:01 +0100

    AddCommentActivity Basic

--- app/src/main/java/h_da/fbi/khami/photostream/AddCommentActivity.java
+++ app/src/main/java/h_da/fbi/khami/photostream/AddCommentActivity.java
@@ -1,14 +1,56 @@
 package h_da.fbi.khami.photostream;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-public class AddCommentActivity extends AppCompatActivity
+import hochschuledarmstadt.photostream_tools.IPhotoStreamClient;
+import hochschuledarmstadt.photostream_tools.PhotoStreamActivity;
+
+public class AddCommentActivity extends PhotoStreamActivity
 {
+    IPhotoStreamClient photoStreamClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_comment);
+
+        photoStreamClient = getPhotoStreamClient();
+
+
+    }
+
+    @Override
+    protected void onPhotoStreamServiceConnected(IPhotoStreamClient photoStreamClient, Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    protected void onPhotoStreamServiceDisconnected(IPhotoStreamClient photoStreamClient) {
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_comment_add, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+
+            case R.id.post_comment_item:
+                //uploadComment(1,"sdf");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
